232545a09a2dc9c8aac08364e6e5d6b848de7755
Ashley Wulber <ashley@system76.com>
2022-08-11 09:44:29 -0400

    fix: refactor file chooser usage so that it works better for the flatpak

--- src/components/theme_chooser_button/mod.rs
+++ src/components/theme_chooser_button/mod.rs
@@ -24,41 +24,56 @@ impl ThemeChooserButton {
         let imp = imp::ThemeChooserButton::from_instance(&self);
         imp.button.borrow().connect_clicked(
             glib::clone!(@weak imp.file_chooser as file_chooser, @weak self as self_ => move |_| {
-                let file_chooser = file_chooser.borrow();
+                let window = self_
+                    .root()
+                    .map(|root| {
+                        if let Ok(w) = root.downcast::<Window>() {
+                            Some(w)
+                        } else {
+                            None
+                        }
+                    })
+                    .unwrap_or_default();
+
+                let file_chooser = FileChooserNative::new(
+                    Some("Select Theme"),
+                    window.as_ref(),
+                    gtk4::FileChooserAction::Open,
+                    None,
+                    None,
+                );
+
                 let xdg_dirs = xdg::BaseDirectories::with_prefix(NAME).unwrap();
                 let mut path: PathBuf = xdg_dirs.get_data_home();
                 path.push(THEME_DIR);
                 let _ = file_chooser.set_current_folder(Some(&File::for_path(path)));
-                file_chooser.show();
-            }),
-        );
-    }
+                file_chooser.connect_response(
+                    glib::clone!(@weak self_ => move |file_chooser, response| {
+                        if response != gtk4::ResponseType::Accept {return};
+                        if let Some(f) = file_chooser.file() {
+                            self_.emit_by_name::<()>("file-selected", &[&f]);
+                        }
+                    }),
+                );
 
-    fn connect_file_chooser(&self) {
-        let imp = imp::ThemeChooserButton::from_instance(&self);
-        imp.file_chooser.borrow().connect_response(
-            glib::clone!(@weak self as self_ => move |file_chooser, response| {
-                if response != gtk4::ResponseType::Accept {return};
-                if let Some(f) = file_chooser.file() {
-                    self_.emit_by_name::<()>("file-selected", &[&f]);
-                }
+                let filter = gtk4::FileFilter::new();
+                filter.add_suffix("ron");
+                file_chooser.add_filter(&filter);
+
+                file_chooser.show();
+                let imp = imp::ThemeChooserButton::from_instance(&self_);
+                imp.file_chooser.replace(file_chooser);
             }),
         );
     }
 
     pub fn new() -> Self {
-        let button = cascade! {
-            Button::with_label("Load theme");
-            ..add_css_class("background-component");
-            ..add_css_class("padding-medium");
-            ..add_css_class("border-radius-medium");
-        };
+        let button = Button::with_label("Load theme");
 
         let self_: Self = glib::Object::new(&[]).expect("Failed to create `ThemeChooserButton`.");
         cascade! {
             &self_;
             ..append(&button);
-            ..add_css_class("background");
             ..set_margin_top(4);
             ..set_margin_bottom(4);
             ..set_margin_start(4);
@@ -67,34 +82,9 @@ impl ThemeChooserButton {
         };
         let imp = imp::ThemeChooserButton::from_instance(&self_);
 
-        let window = self_
-            .root()
-            .map(|root| {
-                if let Ok(w) = root.downcast::<Window>() {
-                    Some(w)
-                } else {
-                    None
-                }
-            })
-            .unwrap_or_default();
-
-        let file_chooser = FileChooserNative::new(
-            Some("Select Theme"),
-            window.as_ref(),
-            gtk4::FileChooserAction::Open,
-            None,
-            None,
-        );
-
-        let filter = gtk4::FileFilter::new();
-        filter.add_suffix("ron");
-        file_chooser.add_filter(&filter);
-
         imp.button.replace(button);
-        imp.file_chooser.replace(file_chooser);
 
         self_.connect_button_to_chooser_dialog();
-        self_.connect_file_chooser();
 
         self_
     }
